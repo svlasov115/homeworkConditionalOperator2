9c9bc42f906df4ccd30ab52f0c69cee1bdfa8a3d
Семён <svlasov115@vk.com>
2024-02-03 23:07:39 +0500

    Added task 1 solving

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
+        // Задача 1
+        System.out.println("Задача 1");
 
+        int clientOS = 1;
+
+        if (clientOS == 0) {
+            System.out.println("Установите версию приложения для iOS по ссылке.");
+        } else {
+            System.out.println("Установите версию приложения для Android по ссылке.");
+        }
     }
 }
